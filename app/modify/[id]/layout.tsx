bcde89d93b52ca0090504c2eeb392b72c60827eb
rhtjddls123 <jjjk0605@naver.com>
2024-01-29 19:53:22 +0900

    수정할때 권한 없으면 redirect부분 layout으로 옮김

---
+++ app/modify/[id]/layout.tsx
@@ -0,0 +1,29 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { connectDB } from '@/util/database';
+import { postType } from '@/util/typs';
+import { ObjectId } from 'mongodb';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: Readonly<{
+    children: React.ReactNode;
+  }>;
+  params: { id: string };
+}) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect('/list');
+  }
+  const db = (await connectDB).db('forum');
+  const result = (await db
+    .collection('post')
+    .findOne({ _id: new ObjectId(params.id) })) as postType;
+
+  if (session.user.role !== 'admin' && result.author !== session.user.email)
+    redirect('/list');
+  return <>{children}</>;
+}
